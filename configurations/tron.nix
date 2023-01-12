916241ab049e4d221248680b1033652de460631a
Adam Parak <definitelynotmymail@gmail.com>
2023-01-12 12:21:07 +0100

    Added modesetting, fixed missing exec ~tron

--- configurations/tron.nix
+++ configurations/tron.nix
@@ -27,14 +27,15 @@ in {
     ];
   };
   environment.systemPackages = [nvidia-offload];
-  hardware.nvidia.prime = {
-    offload.enable = true;
-
-    intelBusId = "PCI:0:2:0";
-
-    nvidiaBusId = "PCI:1:0:0";
-
+  hardware.nvidia = {
     modesetting.enable = true;
+    prime = {
+      offload.enable = true;
+
+      intelBusId = "PCI:0:2:0";
+
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   # Use the systemd-boot EFI boot loader.
