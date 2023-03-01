ab2d76259901b63627c32ced5a67f24fc55a7509
Adam Parak <definitelynotmymail@gmail.com>
2023-03-01 15:42:14 +0100

    Add new work HW ~Geryanos

---
+++ configurations/geryones.nix
@@ -0,0 +1,38 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  hardware.opengl = {
+    enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    gnumake
+    gcc
+    pciutils
+    file
+  ];
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["ntfs"];
+
+  services.xserver.wacom.enable = true;
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.useDHCP = false;
+  networking.interfaces.wlo1.useDHCP = true;
+
+  imports = [./geryones-hw.nix];
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+}
