6441e813b279f1913d95a2059a7f613eca9a8d5f
Adam Parak <definitelynotmymail@gmail.com>
2023-03-01 10:31:41 +0100

    Updates to GNOME

--- modules/gui.nix
+++ modules/gui.nix
@@ -16,9 +16,28 @@
 
     libinput.enable = true;
   };
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [ gnome.adwaita-icon-theme gnomeExtensions.appindicator ];
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 
-  environment.systemPackages = with pkgs; [
-  ];
+  environment.gnome.excludePackages = (with pkgs; [
+  #  gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+  #  gnome-terminal
+    gedit # text editor
+  #  epiphany # web browser
+    geary # email reader
+  #  evince # document viewer
+    gnome-characters
+    totem # video player
+  #  tali # poker game
+  #  iagno # go game
+  #  hitori # sudoku game
+  #  atomix # puzzle game
+  ]);
 
   # Enable sound with pipewire.
   sound.enable = true;
