8e77adca03de31d5c1238d2bfb10d4f44ed6df5e
Adam Parak <xparak1@fi.muni.cz>
2023-03-09 20:22:40 +0100

    update to nixvim

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,14 +1,41 @@
-{
-  config,
-  pkgs,
-  nixvim,
-  ...
-}: let
+{ config
+, pkgs
+, nixvim
+, system
+, ...
+}:
+let
   config = {
     colorschemes.gruvbox.enable = true;
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          clangd.enable = true;
+          eslint.enable = true;
+          denols.enable = true;
+          html.enable = true;
+          jsonls.enable = true;
+          pylsp.enable = true;
+          rnix-lsp.enable = true;
+          tailwindcss.enable = true;
+          tsserver.enable = true;
+          vuels.enable = true;
+        };
+      };
+      telescope = {
+        enable = true;
+        extensions = {
+            frecency.enable = true;
+            fzf-native.enable = true;
+        };
+      };
+    };
   };
-  nvim = nixvim.makeNixvim config;
-in {
+  nixvim' = nixvim.legacyPackages.${system};
+  nvim = nixvim'.makeNixvim config;
+in
+{
   environment.systemPackages = [
     nvim
   ];
