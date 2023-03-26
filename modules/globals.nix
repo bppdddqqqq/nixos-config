cdc0aad9dab9fd4f3c74ecc20cedad8fd96b83e1
Adam Parak <xparak1@fi.muni.cz>
2023-03-26 12:31:36 +0200

    enable android sdk, reenable cronjob

--- modules/globals.nix
+++ modules/globals.nix
@@ -5,7 +5,11 @@
 }: {
   # nix settings
   nixpkgs.config.allowUnfree = true;
+  # android sdk
+  nixpkgs.config.android_sdk.accept_license = true;
+  # dedupe
   nix.settings.auto-optimise-store = true;
+  # nix flakes activation
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # default shell is FISH
@@ -15,15 +19,16 @@
   # enable documentation
   documentation.dev.enable = true;
 
+  # fonts!
   fonts.fonts = with pkgs; [
     (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono"];})
     #    google-fonts
   ];
 
   # clean logs older than 2d
-#  services.cron.systemCronJobs = [
-#    "0 20 * * * root journalctl --vacuum-time=2d"
-#  ];
+  services.cron.systemCronJobs = [
+    "0 20 * * * root journalctl --vacuum-time=2d"
+  ];
 
   # set state version
   system.stateVersion = "22.11";
