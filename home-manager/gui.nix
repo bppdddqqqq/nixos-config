456ba4776955a1941bcc7b8357d97f6db651913e
Adam Parak <xparak1@fi.muni.cz>
2023-05-30 00:54:08 +0200

    fix mime types

--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -159,6 +159,56 @@ in
   #gtk stuff
   gtk.iconTheme.package = pkgs.gnome.adwaita-icon-theme;
   gtk.iconTheme.name = "Adwaita";
+  xdg = {
+    enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # codium
+        "application/json" = "codium.desktop";
+        "application/ld+json" = "codium.desktop";
+        "application/javascript" = "codium.desktop";
+        "inode/directory" = "codium.desktop";
+        "text/plain" = "codium.desktop";
+        "text/x-csrc" = "codium.desktop";
+        "application/x-php" = "codium.desktop";
+        "application/x-yaml" = "codium.desktop";
+
+        # gnome defaults
+        "image/*" = "org.nomacs.ImageLounge.desktop";
+        "image/png" = "org.nomacs.ImageLounge.desktop";
+        "image/jpeg" = "org.nomacs.ImageLounge.desktop";
+        "image/tiff" = "org.nomacs.ImageLounge.desktop";
+        "image/webp" = "org.nomacs.ImageLounge.desktop";
+        "image/avif" = "org.nomacs.ImageLounge.desktop";
+        "image/bmp" = "org.nomacs.ImageLounge.desktop";
+        "image/gif" = "org.nomacs.ImageLounge.desktop";
+        
+        "video/*" = "vlc.desktop";
+        "audio/*" = "mplayer.desktop";
+        "application/pdf" = "org.gnome.Evince.desktop";
+
+        # apps mumbojumbo
+        "x-scheme-handler/tg" = "telegramdesktop.desktop";
+        "x-scheme-handler/postman" = "Postman.desktop";
+
+        # firefox
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/ftp" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
+      };
+    };
+  };
   programs.firefox.enable = true;
   programs.firefox.package = pkgs.firefox.override {
     # See nixpkgs' firefox/wrapper.nix to check which options you can use
